01ec87c1f44062aa71c8fd52571c0d2aa32b7cd6
Admin <manjot.gni@gmail.com>
2017-03-01 08:21:57 +0530

    Initial Commit , started learning VB .net

---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        TextBox1.Text = "Hello ! This is my first Program"
+        TextBox1.TextAlign = HorizontalAlignment.Center
+
+    End Sub
+End Class
