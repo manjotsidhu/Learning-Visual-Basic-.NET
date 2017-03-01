bbcf24a6735fd2a2ce7ac50426d2f516211fa6cd
Admin <manjot.gni@gmail.com>
2017-03-01 09:08:48 +0530

    Set Environment For Form2 v2

--- WindowsApplication1/Form2.Designer.vb
+++ WindowsApplication1/Form2.Designer.vb
@@ -25,11 +25,12 @@ Partial Class Form2
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(98, 169)
+        Me.Button1.Location = New System.Drawing.Point(97, 150)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
@@ -38,7 +39,7 @@ Partial Class Form2
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(122, 95)
+        Me.TextBox1.Location = New System.Drawing.Point(122, 90)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(24, 20)
         Me.TextBox1.TabIndex = 1
@@ -52,11 +53,20 @@ Partial Class Form2
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Please Enter Any Number"
         '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(52, 179)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(174, 20)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "Your Output Will be Displayed Here"
+        '
         'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
@@ -69,4 +79,5 @@ Partial Class Form2
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
 End Class
