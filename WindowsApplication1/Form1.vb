04b96346d75ce1771960bd99f5e3f836bf7e1360
Admin <manjot.gni@gmail.com>
2017-03-01 08:29:21 +0530

    Learnt Declaring Variables

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -3,6 +3,10 @@
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         TextBox1.Text = "Hello ! This is my first Program"
         TextBox1.TextAlign = HorizontalAlignment.Center
-
+    End Sub
+    Sub Main()
+        Dim mnj As String
+        mnj = "My Name Is Manjot Sidhu"
+        System.Console.WriteLine(mnj)
     End Sub
 End Class
