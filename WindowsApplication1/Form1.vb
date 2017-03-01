9d75f4d1727fe41106d61cb66581c77670babe48
Admin <manjot.gni@gmail.com>
2017-03-01 08:38:28 +0530

    Linked Section to button for output

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -4,9 +4,10 @@
         TextBox1.Text = "Hello ! This is my first Program"
         TextBox1.TextAlign = HorizontalAlignment.Center
     End Sub
-    Sub Main()
+
+    Private Sub my1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         Dim mnj As String
         mnj = "My Name Is Manjot Sidhu"
-        System.Console.WriteLine(mnj)
+        TextBox1.Text = mnj
     End Sub
 End Class
