9104757dccc8f49a7eecd98a827bbd9278a61b05
Admin <manjot.gni@gmail.com>
2017-03-01 08:46:25 +0530

    Some

--- WindowsApplication1/Form1.Designer.vb
+++ WindowsApplication1/Form1.Designer.vb
@@ -28,9 +28,9 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(104, 91)
+        Me.Button1.Location = New System.Drawing.Point(79, 88)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(124, 23)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Click Me"
         Me.Button1.UseVisualStyleBackColor = True
--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -3,12 +3,14 @@
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         TextBox1.Text = "Hello ! This is my first Program"
         TextBox1.TextAlign = HorizontalAlignment.Center
+        Button1.Text = "Press Me"
     End Sub
 
     Private Sub my1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         Dim mnj As String
         mnj = "My Name Is Manjot Sidhu"
         TextBox1.Text = mnj
+        Button1.Text = "Thank You"
     End Sub
 
     Private Sub my2(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.Click
